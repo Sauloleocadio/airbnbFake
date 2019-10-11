b9e2a562ff8d1a1dcdad44826c55f6dd84412afe
Sauloleocadio <saulomanshu@gmail.com>
2019-10-11 17:35:35 -0300

    first commit

---
+++ src/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const routes = require('./routes');
+
+mongoose.connect('mongodb+srv://omnistack:omnistack@oministack-emg8y.mongodb.net/semana09?retryWrites=true&w=majority',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+
+const app = express();
+
+// GET , POST , PUT , DELETE
+//req.query = Acessar query params (para filtros)
+//req.params = Acessar route params (para edição,deletes)
+//req.body =  Acessar o corpo da requisição (para criaçao e ediçao)
+app.use(express.json());
+app.use(routes);
+
+
+
+app.listen(3333);
